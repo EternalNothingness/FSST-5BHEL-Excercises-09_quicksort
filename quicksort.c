871687bfd80188e3f3565fb2a5c7d0f794a6dcde
EternalNothingness <66483318+EternalNothingness@users.noreply.github.com>
2020-12-22 22:36:24 +0100

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -2,22 +2,48 @@
 Titel: quicksort
 Beschreibung:
 Autor: Patrick Wintner
-GitHub: https://github.com/EternalNothingness/FSST-5BHEL-Excercises-09_quicksort.git
+GitHub: https://github.com/EternalNothingness/FSST-5BHEL-Excercises-07_quicksort.git
 Datum der letzten Bearbeitung: 22.12.2020
 */
 
-// -- Standardbibliotheken --
-#include <stdlib.h>
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// -- Dateiverwaltungsbibliotheken --
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/uio.h>
-#include <unistd.h>
+void qs(int *a, int us, int os)
+{
+	// TODO
+}
 
-// -- andere Bibliotheken --
-#include <string.h>
-#include <sys/time.h>
-#include <stdbool.h> 
+// creates a array of size size and fills it with random ints in range 0 to max_int
+int *create_array(int size, int max_int)
+{
+	int *b = (int*)malloc(size * sizeof(int));
+
+	for (int i=0; i<size; i++) {
+		b[i] = rand() % max_int;
+	}
+
+	return b;
+}
+
+#define MY_SIZE 32
+
+int main(int argc, char **argv)
+{
+	// create random ints based in current time
+	srand(time(NULL));
+
+        int *a = create_array(MY_SIZE, 100);
+
+	qs(a, 0, MY_SIZE);
+
+	int old = -1;
+	for (int i=0; i<MY_SIZE; ++i)      {
+		if (old != -1) assert(old <= a[i]);
+		printf("%d ", a[i]);
+		old = a[i];
+	}
+	printf("\n");
+}
